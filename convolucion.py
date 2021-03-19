f8bd48b5efd9e221b7665a2e00577e3fcc79cade
ArielAlvarezCortez <a01378734@itesm.mx>
2021-03-19 08:34:43 -0600

    nuevos archivos

--- convolucion.py
+++ convolucion.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cv2
 #Ioriginal = matriz original
 def convolucion(Ioriginal, kernel):
     '''Método encargado de realizar una convolución a una imagen
@@ -11,7 +11,7 @@ def convolucion(Ioriginal, kernel):
     #fr - filas, cr - columnas
     fr = len(Ioriginal)-(len(kernel)-1)
     cr = len(Ioriginal[0])-(len(kernel[0])-1)
-    res = np.zeros((fr, cr))
+    res = np.zeros((fr, cr), np.uint8)
 
     #filas, matríz resultado
     for i in range(len(res)):
@@ -23,7 +23,11 @@ def convolucion(Ioriginal, kernel):
                 #columnas, kernel
                 for n in range(len(kernel[0])):
                     suma += kernel[m][n] * Ioriginal[m+i][n+j]
-            res[i][j] = suma
+            if suma<=255:
+                res[i][j]=round(suma)
+            else:
+                res[i][j]=255
+
     return res
 
 K = [[-1, 0, 1],[-1, 0, 1],[-1, 0, 1]]#kernel
@@ -33,6 +37,11 @@ I = [[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]#prototipo de i
 In = np.array(I)
 Kn = np.array(K)
 
+IRGB = cv2.imread('004.jpg')
+IGS= cv2.cvtColor(IRGB, cv2.COLOR_BGR2GRAY)
+print(IGS.shape)
 #llamado de la función e impresión del resultado
-R = convolucion(In, Kn)
+R = convolucion(IGS, Kn)
 print(R)
+print(R.shape)
+cv2.imwrite('004C.jpg',R)

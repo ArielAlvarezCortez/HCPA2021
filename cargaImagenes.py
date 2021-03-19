2523b260a40a488f401545dfc270046516bf2cf1
ArielAlvarezCortez <40802944+ArielAlvarezCortez@users.noreply.github.com>
2021-03-19 08:04:10 -0600

    Cambio rama2

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -6,7 +6,5 @@ IRGB = cv2.imread('r2d2.jpg')
 print(IRGB)
 print(IRGB.shape)
 print("lineas agregadas rama2")
-IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
-print(IGS)
-print(IGS.shape)
+print(len(IRGB))
 
